db63eed3b7bc8bf5c492bb652cbbed4b12f93758
Marcus Gajozo <marcusgajozo@gmail.com>
2022-12-19 13:14:59 -0400

    project created

---
+++ src/test/java/com/aulaspringboot/aulaSpringBoot/AulaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aulaspringboot.aulaSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
